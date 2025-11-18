a45e1d3336d265d76861508f0341c451de24c6dd
vietha95 <vietha4895@gmail.com>
2025-11-18 13:26:48 +0000

    Acceptance test

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -4,8 +4,16 @@ describe('Shows Appeteasers', () => {
   });
   it('selects Handhelds', () => {
     //TODO: Add a test to check Handhelds
+    cy.contains('Handhelds').click();
+    cy.contains('Cheese Burger');
+    cy.contains('Fajita Tacos');
   });
   it('selects Appeteasers', () => {
     //TODO: Add a test to check Appeteasers
+    cy.contains('Appeteasers').click();
+    cy.contains('Tater Tots');
+    cy.contains('Buffalo Wings');
+    cy.contains('Cheese Burger').should('not.exist');
+    cy.contains('Fajita Tacos').should('not.exist');
   });
 });
